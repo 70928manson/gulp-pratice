fa7d435bc00d4d1873ddb73034421f97bbcf1de5
Manson <manson972@yahoo.com>
2022-08-14 20:45:53 +0800

    update products page and adjust css with sass

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,34 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+Array.from(document.querySelectorAll("#tabs-list li")).forEach(item => item.addEventListener("click", function (e) {
+  console.log('test');
+  // e.preventDefault();
+  let link = this.getAttribute("data-link");
+  document.querySelector('#tabs-list .active').classList.remove("active");
+
+  if(link === 'optical'){
+    console.log('1');
+    document.querySelector("[data-link='optical']").classList.add("active");
+    hideAll();
+    document.querySelector("[data-target='optical']").classList.remove("d-none");
+
+  }else if(link === 'sunglasses'){
+    console.log('2');
+    document.querySelector("[data-link='sunglasses']").classList.add("active");
+    hideAll();
+    document.querySelector("[data-target='sunglasses']").classList.remove("d-none");
+    
+  }else if(link === 'functional'){
+    console.log('3');
+    document.querySelector("[data-link='functional']").classList.add("active");
+    hideAll();
+    document.querySelector("[data-target='functional']").classList.remove("d-none");
+  }
+
+  function hideAll() {
+    Array.from(document.querySelectorAll("section")).forEach(sec => sec.classList.add("d-none"));
+  }
+})
+);
